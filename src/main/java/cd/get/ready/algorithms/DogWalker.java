d9f57a1c0ff8b117ece7de69d43abdaa754d4ff4
Heather <heather.craddock@hotmail.co.uk>
2020-09-28 22:57:42 -0400

    solution

--- src/main/java/cd/get/ready/algorithms/DogWalker.java
+++ src/main/java/cd/get/ready/algorithms/DogWalker.java
@@ -1,8 +1,41 @@
 package cd.get.ready.algorithms;
 
+import java.util.HashMap;
+
 public class DogWalker {
+
     public int numberOfWalksCalculator(int[] dogSizes){
-        int numberOfWalks = -1;
+        int numberOfWalks = 0;
+        //create new hashtable dogPairs of type <int, int>
+        HashMap<Integer, Integer> dogPairs = new HashMap<>();
+
+        // loop through the dog sizes array
+        for(int d: dogSizes) {
+            System.out.println(d);
+            // if key exists in dogPairs
+            if(dogPairs.containsKey(d)){
+                // remove that key value pair
+                System.out.println("exists, remove");
+                dogPairs.remove(d);
+            } else {
+                //add the key value pair and increment the numberOfWalks
+                dogPairs.put(d, 1);
+                numberOfWalks++;
+                System.out.println("doesn't exist, add");
+            }
+            System.out.printf("Number of walks: %d %n", numberOfWalks);
+        }
+
         return numberOfWalks;
     }
+
+// If two dogs are the same size, they can be walked in one hour
+// 3 of the same size take 2 hours?
+    /*
+    using a hashmap:
+    - use the size as the key, and a count as the value
+    - if the key doesn't exist, increment the number of hours.
+    - if the key doesn't exist, remove the key/ value pair and don't increment hours
+
+     */
 }
--- src/test/java/cd/get/ready/algorithms/DogWalkerTest.java
+++ src/test/java/cd/get/ready/algorithms/DogWalkerTest.java
@@ -3,7 +3,7 @@ package cd.get.ready.algorithms;
 import org.junit.Assert;
 import org.junit.Test;
 
-class DogWalkerTest {
+public class DogWalkerTest {
 
     @Test
     public void testOne(){
@@ -17,9 +17,19 @@ class DogWalkerTest {
     @Test
     public void testTwo(){
         DogWalker dw = new DogWalker();
-        int[] dogs = {10, 20, 10, 10, 30, 50, 10, 20, 30};
+        int[] dogs = {10, 20, 10, 10, 30, 50, 10, 20, 30, 100};
         int expected = dw.numberOfWalksCalculator(dogs);
         int actual = 6;
+        Assert.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void testThree(){
+        DogWalker dw = new DogWalker();
+        int[] dogs = {1, 1, 2, 2, 2, 2, 3, 4, 4, 4, 5, 5, 5};
+        int expected = dw.numberOfWalksCalculator(dogs);
+        int actual = 8;
         Assert.assertEquals(expected, actual);
     }
 
